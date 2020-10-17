e8510c5a59ac4f1217e2b84468a0af95fd71f6d8
blackalbinodee <blackalbinodee@gmail.com>
2020-10-17 09:47:27 +0100

    Update Challenge3.Js
    my submission

--- Challenge3.js
+++ Challenge3.js
@@ -15,3 +15,10 @@ Output Example:
 
 Put your answer below! This would be discussed in our weekly meeting on Saturday! Please complete and hand in before then so i can review for feedback! Complete feedback would only be given to those at the meeting on saturday! Take care :) */
 
+function reverse_a_number(n)
+{
+    n = n + "";
+    return n.split("").reverse().join("");
+}
+console.log(reverse_a_number(976540));
+//output= 045679
